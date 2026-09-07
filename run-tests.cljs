e289a9d836c7d536eb78728ecfb9cab9083dd42d
Jun Kawasaki <jun784@gmail.com>
2026-09-07 10:06:03 +0900

    q9 pilot: add run-tests.cljs (portable-half nbb runner)

---
+++ run-tests.cljs
@@ -0,0 +1,11 @@
+(ns run-tests
+  (:require [cljs.test :as t]
+            [agones-compat.main-test]))
+
+(defmethod t/report [:cljs.test/default :end-run-tests] [m]
+  (println (str "\nnbb: " (:test m) " tests, " (:pass m) " passed, "
+                (:fail m) " failed, " (:error m) " errors"))
+  (when (pos? (+ (or (:fail m) 0) (or (:error m) 0)))
+    (set! (.-exitCode js/process) 1)))
+
+(t/run-tests 'agones-compat.main-test)
